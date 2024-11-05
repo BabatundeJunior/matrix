6686c18820c4f14a14a6ca5815d7c99c7e82bbd7
BabatundeJunior <jbabs128@gmail.com>
2024-11-05 15:01:24 +0100

    initial commit

---
+++ authentication/admin.py
@@ -0,0 +1,10 @@
+# from django.contrib import admin
+#
+# from .models import UserProfile, Subscription
+#
+# # Register your models here.
+#
+# admin.site.register(UserProfile)
+# # admin.site.register(Enrollment)
+# # admin.site.register(Progress)
+# admin.site.register(Subscription)
---
+++ core/admin.py
@@ -0,0 +1,12 @@
+# from django.contrib import admin
+#
+# from .models import Course, Module, Lesson, Quiz, Question, Choice
+#
+# # Register your models here.
+#
+# admin.site.register(Course)
+# admin.site.register(Module)
+# admin.site.register(Lesson)
+# admin.site.register(Quiz)
+# admin.site.register(Question)
+# admin.site.register(Choice)
